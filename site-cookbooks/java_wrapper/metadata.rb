ebfbc5b8c9e9955dafdf0df72459411b1337304f
Luis Alves <lmtjalves@hotmail.com>
2015-08-27 16:48:20 +0100

    Identation...

--- site-cookbooks/java_wrapper/metadata.rb
+++ site-cookbooks/java_wrapper/metadata.rb
@@ -1,6 +1,6 @@
 name             'java_wrapper'
 maintainer       'Passworks'
-maintainer_email ''
+maintainer_email 'luis.alves@passworks.io'
 license          'All rights reserved'
 description      'Java cookbook wrapper to install java 8'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
