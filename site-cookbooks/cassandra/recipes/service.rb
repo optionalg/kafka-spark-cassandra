f69cb08c3ca5479440c5d24234cc444bb160a450
Luis Alves <lmtjalves@hotmail.com>
2015-09-08 12:19:13 +0100

    Cassandra is now using upstart

--- site-cookbooks/cassandra/recipes/service.rb
+++ site-cookbooks/cassandra/recipes/service.rb
@@ -6,9 +6,23 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# See https://supermarket.chef.io/cookbooks/monit_wrapper
-include_recipe "monit_wrapper"
+cassandra_user  = node["cassandra"]["user"]
 
+# init script for spark jobserver
+template "etc/init/cassandra.conf" do
+	source "cassandra.init.erb"
+	owner cassandra_user
+	action :create
+	mode 770
+	notifies :restart, "service[cassandra]", :delayed
+	variables(
+		:cassandra_user => cassandra_user
+	)
+end
+
+# Setup the jobserver service with upstart
 service "cassandra" do
+	provider Chef::Provider::Service::Upstart
+	supports :status => true, :restart => true, :stop => true
 	action [:enable, :start]
 end
